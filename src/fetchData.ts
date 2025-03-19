849de5fefb5ca7e8420413b383f564e2b55cd012
jaysojitra13 <jaysojitra13@gmail.com>
2025-03-19 21:07:58 +0530

    fix: update s3 url

--- src/fetchData.ts
+++ src/fetchData.ts
@@ -4,7 +4,7 @@ import { Data } from "./types";
 export async function readFile(): Promise<Data[]> {
   try {
     const response = await axios.get(
-      "https://klink-assets.s3.eu-central-1.amazonaws.com/klink-contract-lifi/lifi-data.json"
+      "https://assets.klink.finance/klink-contract-lifi/lifi-data.json"
     );
 
     const data: any = await response.data;
